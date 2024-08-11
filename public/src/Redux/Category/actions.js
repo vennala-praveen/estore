eccd4a9025884676bae2c049e593a7517640dde2
vennala praveen <praveen98vennala.com>
2024-08-11 14:58:50 +0530

    some files added

---
+++ public/src/Redux/Category/actions.js
@@ -0,0 +1,10 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+
+export const getCategories = createAsyncThunk(
+    'getCategories',
+    ()=>{
+        const categories = ["Men","Women","Kids","Best Offers","All"];
+        return categories;
+    }
+)
--- src/Redux/Product/productSlice.js
+++ src/Redux/Product/productSlice.js
@@ -1,48 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const initialState = [
     {
         pName:"Jacket",
-        price:90,
+        price:45,
         img:"shop-1.jpg"
     },
-
     {
         pName:"Purse",
-        price:30,
+        price:50,
         img:"shop-2.jpg"
     },
-
     {
         pName:"Dress",
-        price:40,
+        price:38,
         img:"shop-3.jpg"
     },
-
     {
         pName:"Denim",
-        price:60,
+        price:42,
         img:"shop-4.jpg"
     },
-
     {
         pName:"Boots",
-        price:190,
+        price:65,
         img:"shop-5.jpg"
     },
-
     {
         pName:"Bag",
-        price:120,
+        price:35,
         img:"shop-6.jpg"
     }
+
 ]
 
 const productSlice = createSlice({
-    name:"Products",
+    name: "Products",
     initialState
-
 })
 
 export default productSlice;
